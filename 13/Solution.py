2fa9920adf79f086479890b03d57f2723eddc7a1
Swapnil Dhamu <swapnildhamu@Swapnils-MacBook-Pro.local>
2023-12-27 03:24:30 +0530

    Solved day 13 (updated)
    Signed-off-by: Swapnil Dhamu <swapnildhamu@Swapnils-MacBook-Pro.local>

---
+++ 13/Solution.py
@@ -0,0 +1,23 @@
+import sys
+from pathlib import Path
+
+
+def find(pttr, tol=0):
+    for i in range(1, len(pttr)):
+        if tol == sum(
+            c != d
+            for a, b in zip(pttr[i - 1 :: -1], pttr[i:])
+            for c, d in zip(a, b)
+        ):
+            return i
+    return False
+
+
+def solve(k, t=0):
+    return sum((100 * find(p, t)) or find(list(zip(*p)), t) for p in k)
+
+
+if __name__ == "__main__":
+    k = list(map(str.split, Path(sys.argv[1]).read_text().split("\n\n")))
+    print("Part 1:", solve(k))
+    print("Part 2:", solve(k, 1))
